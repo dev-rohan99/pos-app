b5af6217286334158871797719c406d31cf128f6
dev-rohan99 <rohanmostofa599@gmail.com>
2026-01-28 22:52:56 +0600

    feat(dev): added create controller endpoint and fixed products module issuesgit add .

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,16 +3,19 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
+      global: true,
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1d' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtAuthGuard],
   controllers: [AuthController],
+  exports: [JwtModule, JwtAuthGuard],
 })
 export class AuthModule {}
--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  create(@Body() dto: CreateProductDto) {
+    return this.productsService.create(dto);
+  }
 }
