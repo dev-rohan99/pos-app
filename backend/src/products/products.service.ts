9e9059c21286bde7b813beba376daeef25deb5d2
dev-rohan99 <rohanmostofa599@gmail.com>
2026-01-28 22:24:22 +0600

    feat(dev): added create product service logic!

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,7 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
+
+  async create(dto: CreateProductDto) {
+    const existing = await this.prisma.product.findUnique({
+      where: { sku: dto.sku },
+    });
+
+    if (existing) {
+      throw new BadRequestException('SKU already exists');
+    }
+
+    return this.prisma.product.create({
+      data: {
+        name: dto.name,
+        sku: dto.sku,
+        price: dto.price,
+        stockQuantity: dto.stockQuantity,
+      },
+    });
+  }
 }
